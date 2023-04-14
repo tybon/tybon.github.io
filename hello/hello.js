8a8a648f7d46efdd8006fc05d6b7408a65a9ee91
tybon <tjbontrager@willamette.edu>
2023-04-14 03:20:49 -0700

    add hud response, fix other response links

---
+++ hello/hello.js
@@ -0,0 +1,4 @@
+function hello() {
+	val = document.getElementById("the");
+	val.innerText = "hello world";
+}
---
+++ plot/plot.js
@@ -0,0 +1,5 @@
+const plot = document.getElementById("plot");
+
+function f(x) {
+	circle(plot.width/2, plot.height/2, x)
+}
